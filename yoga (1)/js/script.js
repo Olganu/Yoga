3fc41db29860120ea41c84b4cf4c9d343d81cd00
Olga <andrey.plus.olga@gmail.com>
2021-04-14 14:02:51 +0300

    timer

--- yoga (1)/js/script.js
+++ yoga (1)/js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', function () {
-
+// tab
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
@@ -33,4 +33,59 @@ window.addEventListener('DOMContentLoaded', function () {
           }
         }
     });
+
+    //timer
+
+    let deadline = '2021-04-15';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+           // hours = Math.floor((t/(1000*60*60) % 24));
+            //days = Math.floor(t/(1000*60*60*24));
+         return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+         };   
+    }
+
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        timeInterval = setInterval(updaneClock, 1000);
+
+       updaneClock();   // убираем мигание верстки  
+
+        function updaneClock() {
+            let t = getTimeRemaining(endtime);
+
+            
+    function getZero(num) {
+        if(num <= 9) {
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+            hours.textContent =getZero(t.hours);
+            minutes.textContent =getZero(t.minutes);
+            seconds.textContent =getZero(t.seconds);
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
